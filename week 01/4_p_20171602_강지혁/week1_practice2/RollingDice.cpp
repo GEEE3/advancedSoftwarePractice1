e79aa6a2beb913ccbe33b2c17d4a59985f0bf749
GEEE3 <c1as2icblu3@gmail.com>
2021-10-09 23:49:18 +0900

    ✨ Add week 01

---
+++ week 01/4_p_20171602_강지혁/week1_practice2/RollingDice.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+#include "time.h"
+#define _USE_MATH_DEFINES
+#include "math.h"
+
+NOT_USE_MESSAGE
+
+#define MAX_TRY 100
+
+int main()
+{
+	SelectPenObject(RGB(0, 0, 0));
+
+	float hit = 0;
+	float miss = 0;
+	float relProb = 5.0 / 36.0;
+	float prob = 0;
+	
+	// TODO 
+
+	int leftDice, rightDice;
+
+	for (int i = 0; i < MAX_TRY; i++) {
+		leftDice = rand() % 6 + 1;
+		rightDice = rand() % 6 + 1;
+
+		if (leftDice + rightDice == 8) {
+			TextOut(120 * (i / 20), 20 * (i % 20), "try %d: %d,%d (hit!)", 0 + i, leftDice, rightDice);
+			hit += 1;
+		}
+		else {
+			TextOut(120 * (i / 20), 20 * (i % 20), "try %d: %d,%d", 0 + i, leftDice, rightDice);
+			miss += 1;
+		}
+	}
+
+	prob = float(hit / (hit + miss));
+	
+	//
+
+	SetTextColor(RGB(128, 0, 0));
+	// 실제 주사위 값
+	TextOut(0, 420, "실제 값: %.6f", relProb);
+	// 계산된 값
+	TextOut(0, 440, "계산된 값: %.6f", prob);
+
+	ShowDisplay();
+
+	return 0;
+}
